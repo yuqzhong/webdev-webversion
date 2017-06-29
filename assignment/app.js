62fcc2c25c285e71b2dfce04cc614c1550dd6fb8
Yuqing <zhong.yuq@husky.neu.edu>
2017-06-28 23:34:43 -0700

    change app.js

--- assignment/app.js
+++ assignment/app.js
@@ -1,6 +1,19 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-mongoose.connect('mongodb://localhost/zhong-yuqing-webdev');
+// mongoose.connect('mongodb://localhost/zhong-yuqing-webdev');
+
+var connectionString = 'mongodb://localhost/zhong-yuqing-webdev';
+
+if(process.env.MLAB_USERNAME_WEBDEV) {
+    var username = process.env.MLAB_USERNAME_WEBDEV;
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds133271.mlab.com:33271/zhong-yuqing-webdev';
+}
+
+
+
+mongoose.connect(connectionString);
 
 require('./service/user.service.server');
 require('./service/website.service.server');
