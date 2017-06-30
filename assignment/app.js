c2b428987d8d109331723770930a9fc672114e5a
Yuqing <zhong.yuq@husky.neu.edu>
2017-06-30 11:50:05 -0700

    change mongo

--- assignment/app.js
+++ assignment/app.js
@@ -8,7 +8,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) {
     var username = process.env.MLAB_USERNAME_WEBDEV;
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds133271.mlab.com:33271/zhong-yuqing-webdev';
+    connectionString += '@ds133271.mlab.com:37141/heroku_txkg7zr5';
 }
 
 
